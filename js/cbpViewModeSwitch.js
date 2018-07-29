cad651dfa0586c100146d6d4ed1470359968fee1
connie <1049076297@qq.com>
2018-07-29 22:37:23 +0800

    'CPP_website_v2'

--- js/cbpViewModeSwitch.js
+++ js/cbpViewModeSwitch.js
@@ -8,32 +8,32 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-(function() {
+//(function() {
+//
+//	var container = document.getElementById( 'cbp-vm' ),
+//		optionSwitch = Array.prototype.slice.call( container.querySelectorAll( 'div.cbp-vm-options > a' ) );
 
-	var container = document.getElementById( 'cbp-vm' ),
-		optionSwitch = Array.prototype.slice.call( container.querySelectorAll( 'div.cbp-vm-options > a' ) );
+//	function init() {
+//		optionSwitch.forEach( function( el, i ) {
+//			el.addEventListener( 'click', function( ev ) {
+//				ev.preventDefault();
+//				_switch( this );
+//			}, false );
+//		} );
+//	}
 
-	function init() {
-		optionSwitch.forEach( function( el, i ) {
-			el.addEventListener( 'click', function( ev ) {
-				ev.preventDefault();
-				_switch( this );
-			}, false );
-		} );
-	}
+//	function _switch( opt ) {
+//		// remove other view classes and any any selected option
+//		optionSwitch.forEach(function(el) { 
+//			classie.remove( container, el.getAttribute( 'data-view' ) );
+//			classie.remove( el, 'cbp-vm-selected' );
+//		});
+//		// add the view class for this option
+//		classie.add( container, opt.getAttribute( 'data-view' ) );
+//		// this option stays selected
+//		classie.add( opt, 'cbp-vm-selected' );
+//	}
+//
+//	init();
 
-	function _switch( opt ) {
-		// remove other view classes and any any selected option
-		optionSwitch.forEach(function(el) { 
-			classie.remove( container, el.getAttribute( 'data-view' ) );
-			classie.remove( el, 'cbp-vm-selected' );
-		});
-		// add the view class for this option
-		classie.add( container, opt.getAttribute( 'data-view' ) );
-		// this option stays selected
-		classie.add( opt, 'cbp-vm-selected' );
-	}
-
-	init();
-
-})();
+//})();
